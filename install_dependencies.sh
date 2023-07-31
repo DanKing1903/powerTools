653c086c635ffeec2bf9a159eccb8473bedf5c7e
sft-managed <daniel_king@sci-upgrade-2023.c.tamr-gce-poc.internal>
2023-07-31 14:02:08 +0000

    add pyenv

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,3 +1,6 @@
+# checkout submodules
+git submodule update --init --recursive
+
 # install zsh and oh my zsh
 sudo apt install zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -11,3 +14,18 @@ sudo apt-get install libbz2-dev
 sudo apt install stow
 stow -t ~ vim
 stow -t ~/.config ranger
+stow -t ~ pyenv
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
+echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
+echo 'eval "$(pyenv init -)"' >> ~/.zshrc
+stow -t ~/.pyenv/plugins/ pyenv-virtualenv
+echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.zshrc
+__zsh_extras='export EDITOR=vim
+export PYENV_ROOT="$HOME/.pyenv"
+command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+git config --global credential.helper "cache --timeout=3600"
+'
+echo "$__zsh_extras" >> ~/.zshrc
+
