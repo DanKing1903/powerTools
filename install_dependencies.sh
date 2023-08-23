f3c12fc22a9133421bd6458e4990c57b2749ca1d
Ubuntu <ubuntu@sci-upgrade-simulator.c.tamr-gce-poc.internal>
2023-08-23 10:23:08 +0000

    Check for existing pyenv installationwq

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -14,17 +14,22 @@ sudo apt install stow
 stow -t ~ vim
 mkdir -p ~/.config
 stow -t ~/.config ranger
-stow -t ~ pyenv
-echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
+if [ -z "$PYENV_ROOT" ]; then
+    echo "PYENV_ROOT is not set. "
+    stow -t ~ pyenv
+    echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
+    stow -t ~/.pyenv/plugins/ pyenv-virtualenv
+else
+    echo "PYENV_ROOT is set to: $PYENV_ROOT"
+    echo 'export PYENV_ROOT="$PYENV_ROOT"' >> ~/.zshrc
+	cd $PYENV_ROOT
+	git pull
+	cd -
+fi
 echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
 echo 'eval "$(pyenv init -)"' >> ~/.zshrc
-stow -t ~/.pyenv/plugins/ pyenv-virtualenv
 echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.zshrc
 __zsh_extras='export EDITOR=vim
-export PYENV_ROOT="$HOME/.pyenv"
-command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init -)"
-eval "$(pyenv virtualenv-init -)"
 git config --global credential.helper "cache --timeout=3600"
 '
 echo "$__zsh_extras" >> ~/.zshrc
